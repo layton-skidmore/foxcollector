756720f68f6ceae71340f8955da334bbfbc42999
layton-skidmore <layton.skidmore@gmail.com>
2023-08-04 15:53:20 -0500

    part 1 complete / CSS

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
 	path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('foxes/', views.foxes_index, name='index'),
  ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+foxes = [
+  {'name': 'Felix', 'species': 'Arctic Fox', 'description': 'Fearless leader', 'age': 5},
+  {'name': 'Hans', 'species': 'Fennec Fox', 'description': 'Playful little guy and twin of Frans', 'age': 2},
+  {'name': 'Frans', 'species': 'Fennec Fox', 'description': 'Food is his passion. Twin of Hans', 'age': 2},
+]
+
 
 def home(request):
   return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
+
+def foxes_index(request):
+  return render(request, 'foxes/index.html', {
+    'foxes': foxes
+  })
