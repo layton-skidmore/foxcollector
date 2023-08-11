49781caaa5652a66581f8dc7d6d17df2cdf1b3de
layton-skidmore <layton.skidmore@gmail.com>
2023-08-10 19:44:03 -0500

    login-required

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 HABITATS = (
     ('F', 'Forest'),
@@ -23,6 +24,7 @@ class Fox(models.Model):
     description = models.TextField(max_length=250)
     age = models.IntegerField()
     toys = models.ManyToManyField(Toy)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.name} ({self.id})'
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Fox, Toy
 from .forms import HabitatsForm
 from django.urls import reverse
@@ -13,15 +17,16 @@ def about(request):
   return render(request, 'about.html')
 
 
-
+@login_required
 def foxes_index(request):
-    foxes = Fox.objects.all()
+    foxes = Fox.objects.filter(user=request.user)
     return render(request, 'foxes/index.html', 
     { 
         'foxes': foxes 
     }
 )
 
+@login_required
 def foxes_detail(request, fox_id):
   fox = Fox.objects.get(id=fox_id)
   id_list = fox.toys.all().values_list('id')
@@ -32,6 +37,7 @@ def foxes_detail(request, fox_id):
     'toys': toys_fox_doesnt_have
   })
 
+@login_required
 def add_habitats(request, fox_id):
     form = HabitatsForm(request.POST)
     
@@ -41,43 +47,63 @@ def add_habitats(request, fox_id):
         new_habitats.save()
     return redirect('detail', fox_id=fox_id)
 
-class FoxCreate(CreateView):
+class FoxCreate(LoginRequiredMixin, CreateView):
     model = Fox
     fields = ['name', 'species', 'description', 'age']
 
     def get_success_url(self):
         return reverse('detail', kwargs={'fox_id': self.object.id})
+    
+    def form_valid(self, form):
+      form.instance.user = self.request.user 
+      return super().form_valid(form)
 
-class FoxUpdate(UpdateView):
+class FoxUpdate(LoginRequiredMixin, UpdateView):
   model = Fox
   fields = ['species', 'description', 'age']
 
-class FoxDelete(DeleteView):
+class FoxDelete(LoginRequiredMixin, DeleteView):
   model = Fox
   success_url = '/foxes'
 
-class ToyList(ListView):
+class ToyList(LoginRequiredMixin, ListView):
   model = Toy
 
-class ToyDetail(DetailView):
+class ToyDetail(LoginRequiredMixin, DetailView):
   model = Toy
 
-class ToyCreate(CreateView):
+class ToyCreate(LoginRequiredMixin, CreateView):
   model = Toy
   fields = '__all__'
 
-class ToyUpdate(UpdateView):
+class ToyUpdate(LoginRequiredMixin, UpdateView):
   model = Toy
   fields = ['name', 'color']
 
-class ToyDelete(DeleteView):
+class ToyDelete(LoginRequiredMixin, DeleteView):
   model = Toy
   success_url = '/toys'
 
+@login_required
 def assoc_toy(request, fox_id, toy_id):
   Fox.objects.get(id=fox_id).toys.add(toy_id)
   return redirect('detail', fox_id=fox_id)
 
+@login_required
 def unassoc_toy(request, fox_id, toy_id):
   Fox.objects.get(id=fox_id).toys.remove(toy_id)
   return redirect('detail', fox_id=fox_id)
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
